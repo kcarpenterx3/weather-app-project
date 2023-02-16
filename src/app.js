1d24367cdcd024256c7ed38a8c9a35f89dd30924
kcarpenterx3 <katiecx3@gmail.com>
2023-02-16 14:12:10 -0500

    Fixed link and added code for icons to populate.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if(hours < 10) {
+  if (hours < 10) {
     hours = `0${hours}`;
-  } 
+  }
   let minutes = date.getMinutes();
-  if(minutes < 10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   let days = [
@@ -22,13 +22,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-    console.log(response.data)
   let temperatureElement = document.querySelector(".temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -36,10 +36,15 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "72367c58a6b678830274a8ecc81b81e8";
-let city = "fort lauderdale";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&metrics=imperial`;
+let city = "Fort Lauderdale";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&metrics=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
